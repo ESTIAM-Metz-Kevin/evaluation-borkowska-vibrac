d5643c99bb7d81ae4077005f40aa8a59e9305669
Quentin <quent1.vibrac@gmail.com>
2025-01-31 14:55:59 +0100

    Fonction pour créer des buissons

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 import pokemon from "./data/pokemon_4x3.js";
+function cree_buisson(nb_buisson) {
+  const grille = document.querySelector("#grille_de_jeu");
+  for (let i = 0; i < nb_buisson; i++) {
+    let buisson = document.createElement("div");
+    // const select_buisson = grille.querySelector(buisson);
+    let image = document.createElement("img");
+    image.src = "./assets/bush.webp";
 
+    buisson.appendChild(image);
+    grille.appendChild(buisson);
+  }
+}
+cree_buisson(12);
 console.log(pokemon);
