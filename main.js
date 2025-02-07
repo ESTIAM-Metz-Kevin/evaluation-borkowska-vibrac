5d9cd90623a98753a09529063e49b5bac86ed8d0
Quentin <quent1.vibrac@gmail.com>
2025-02-07 10:29:42 +0100

    debut pokeball

--- main.js
+++ main.js
@@ -27,41 +27,84 @@ function disposer_pokemons(nb_memos, pokemon) {
   while (placement_grille.length < nb_memos) {
     let nombre_aleatoire = Math.floor(Math.random() * nb_memos);
     let nb_occurence = 0;
-    for (let i = 0; i < nb_memos; i++) {
-      if (pokemon[nombre_aleatoire]["name"] == placement_grille[i]) {
+    for (let i = 0; i < placement_grille.length; i++) {
+      if (pokemon[nombre_aleatoire]["name"] == placement_grille[i][0]) {
         nb_occurence += 1;
       }
     }
     if (nb_occurence == 0) {
       console.log(nb_occurence);
-      placement_grille.push(pokemon[nombre_aleatoire]["name"]);
-      placement_grille.push(pokemon[nombre_aleatoire]["name"]);
+      placement_grille.push([
+        pokemon[nombre_aleatoire]["name"],
+        pokemon[nombre_aleatoire]["sprite"],
+      ]);
+      placement_grille.push([
+        pokemon[nombre_aleatoire]["name"],
+        pokemon[nombre_aleatoire]["sprite"],
+      ]);
+      nb_occurence += 1;
     }
   }
+
   console.log(placement_grille);
 
   for (let i = 0; i < 20; i++) {
     let indice_aleatoire_1 = Math.floor(Math.random() * nb_memos);
     let indice_aleatoire_2 = Math.floor(Math.random() * nb_memos);
-    let valeur_temporaire = placement_grille[indice_aleatoire_1];
-    placement_grille[indice_aleatoire_1] = placement_grille[indice_aleatoire_2];
-    placement_grille[indice_aleatoire_2] = valeur_temporaire;
+    let valeur_temporaire = placement_grille[indice_aleatoire_1][0];
+    let lien_temporaire = placement_grille[indice_aleatoire_1][1];
+
+    placement_grille[indice_aleatoire_1][0] =
+      placement_grille[indice_aleatoire_2][0];
+    placement_grille[indice_aleatoire_1][1] =
+      placement_grille[indice_aleatoire_2][1];
+
+    placement_grille[indice_aleatoire_2][0] = valeur_temporaire;
+    placement_grille[indice_aleatoire_2][1] = lien_temporaire;
   }
 
   console.log(placement_grille);
   return placement_grille;
 }
 
-function clic() {
+function clic(memo_pokemons) {
+  let nb = 0;
+  let tableau_lance = [];
   const grille = document.querySelector("#grille_de_jeu");
   let division = grille.querySelectorAll("div");
   console.log(division);
-  division.addEventListener("click", function () {
-    console.log("Aaa");
-  });
+
+  for (let i = 0; i < division.length; i++) {
+    let image = division[i].querySelector("img");
+    division[i].onclick = function () {
+      if (nb < 2) {
+        tableau_lance.push(memo_pokemons[i][0]);
+        nb++;
+        image.src = memo_pokemons[i][1];
+        image.style.height = "80px";
+        image.style.width = "auto";
+      }
+      if (nb == 2) {
+        return gagne(tableau_lance);
+      }
+    };
+  }
+}
+
+function gagne(tableau_lance) {
+  console.log(tableau_lance[0], tableau_lance[1]);
+  if (tableau_lance[0] == tableau_lance[1]) {
+    console.log("OK");
+    const grille = document.querySelector("#grille_de_jeu");
+    let division = grille.querySelectorAll("div");
+    let img_pokeball = document.createElement("img");
+    img_pokeball.src = "./assets/pokeball.webp";
+
+    grille.appendChild(img_pokeball);
+  }
 }
 
 creer_buissons(12);
 const memo_pokemons = disposer_pokemons(12, pokemon);
-console.log(pokemon);
-clic();
+
+clic(memo_pokemons);
